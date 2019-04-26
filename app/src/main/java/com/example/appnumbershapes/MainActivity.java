f00aa130ff982e1ce1cbc45b267d178e579a7afe
inkanusmaster <tharkuun@interia.pl>
2019-04-26 10:57:58 +0200

    dodaję metody isTriangular i isSquare

--- app/src/main/java/com/example/appnumbershapes/MainActivity.java
+++ app/src/main/java/com/example/appnumbershapes/MainActivity.java
@@ -3,13 +3,45 @@ package com.example.appnumbershapes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static boolean isTriangularNumber(long num) {
 
+        long calc_num = 8*num+1;
+        long t = (long) Math.sqrt(calc_num);
+
+        if (t*t==calc_num) {
+            System.out.println(+num+ " IS triangular!");
+            return true;
+
+        } else {
+            System.out.println(+num+ " IS NOT triangular!");
+            return false;
+        }
+    }
+
+    public static boolean isSquareNumber(long num) {
+
+        for (int i = 0; i < num / 2 + 2; i++)
+        {
+            if (i * i == num)
+            {
+                System.out.println(+num+ " IS square!");
+                return true;
+            }
+        }
+        System.out.println(+num+ " IS NOT square!");
+        return false;
+    }
 
 
     public void checkNumber(View view){
+
+        EditText enteredNumber = findViewById(R.id.enteredNumberEditText);
+
+
 
     }
 
