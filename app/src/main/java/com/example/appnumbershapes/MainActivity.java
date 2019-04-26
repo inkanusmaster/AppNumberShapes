c24ed6237d2243c54944575a34b14276d78852a5
inkanusmaster <tharkuun@interia.pl>
2019-04-26 13:05:46 +0200

    Dodane metody isSquareNumber i isTriangularNumber. Zrobiony obiekt Number. Zaimplementowany button checkNumber.
    Do sprawdzenia w domu raczej.

--- app/src/main/java/com/example/appnumbershapes/MainActivity.java
+++ app/src/main/java/com/example/appnumbershapes/MainActivity.java
@@ -1,47 +1,64 @@
 package com.example.appnumbershapes;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static boolean isTriangularNumber(int num) {
+    class Number {
 
-        int calc_num = 8*num+1;
-        int t = (int) Math.sqrt(calc_num);
 
-        if (t*t==calc_num) {
-            System.out.println(+num+ " IS triangular!");
-            return true;
+        boolean isTriangularNumber(int num){
 
-        } else {
-            System.out.println(+num+ " IS NOT triangular!");
-            return false;
-        }
-    }
+            int calc_num = 8*num+1;
+            int t = (int) Math.sqrt(calc_num);
 
-    public static boolean isSquareNumber(int num) {
-
-        for (int i = 0; i < num / 2 + 2; i++)
-        {
-            if (i * i == num)
-            {
-                System.out.println(+num+ " IS square!");
+            if (t*t==calc_num) {
                 return true;
+
+            } else {
+                return false;
             }
         }
-        System.out.println(+num+ " IS NOT square!");
-        return false;
+
+        boolean isSquareNumber(int num) {
+
+            for (int i = 0; i < num / 2 + 2; i++){
+                if (i * i == num){
+                    return true;
+                }
+            }
+            return false;
+        }
+
     }
 
 
+    @SuppressLint("SetTextI18n")
     public void checkNumber(View view){
 
         EditText enteredNumberEditText = findViewById(R.id.enteredNumberEditText);
+        int enteredNumber = Integer.parseInt(String.valueOf(enteredNumberEditText));
+        TextView isSquare = findViewById(R.id.isSquareTextView);
+        TextView isTriangular = findViewById(R.id.isTriangularTextView);
 
+        Number number = new Number();
 
+        if (number.isSquareNumber(enteredNumber)){
+            isSquare.setText("Number " +enteredNumber+ "is square!");
+        } else {
+            isSquare.setText("Number " +enteredNumber+ "is NOT square!");
+        }
+
+        if (number.isTriangularNumber(enteredNumber)){
+            isTriangular.setText("Number " +enteredNumber+ "is triangular!");
+        } else {
+            isTriangular.setText("Number " +enteredNumber+ "is NOT triangular!");
+        }
 
     }
 
